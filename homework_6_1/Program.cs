f29a8febf0931f64ad84a1dcb31918749e0da09d
grecheskiy <grecheskiy@rambler.ru>
2023-02-02 00:19:16 +0300

    add homework_6_1

---
+++ homework_6_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+void PrintMatrix(int[,] matr)
+{for (int i=0; i<matr.GetLength(0); i++)
+{for (int j=0; j<matr.GetLength(1); j++)
+{Console.Write($"{matr[i, j]} ");}
+Console.WriteLine();}}
+
+void FillMatrix(int[,] matr)
+{for (int i=0; i<matr.GetLength(0); i++)
+{for (int j=0; j<matr.GetLength(1); j++)
+{matr[i, j] = new Random().Next(10, 100);}}}
+
+int[,] matrix = new int[5, 10];
+PrintMatrix(matrix);
+FillMatrix(matrix);
+Console.Write("-----------------------------");
+Console.WriteLine();
+PrintMatrix(matrix);
