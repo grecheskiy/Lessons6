27f9e0078d3b9e31b0cb843b8946ea7feb0df8ee
grecheskiy <grecheskiy@rambler.ru>
2023-02-02 21:36:27 +0300

    add homework_6_2

---
+++ homework_6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void NewMatrix(int[,] matr)
+{for (int m=0; m<matr.GetLength(0); m++)
+{for (int n=0; n<matr.GetLength(1); n++)
+{matr[m, n] = m + n;}}}
+
+void PrintMatrix(int[,] matr)
+{for (int i=0; i<matr.GetLength(0); i++)
+{for (int j=0; j<matr.GetLength(1); j++)
+{Console.Write($"{matr[i, j]} ");}
+Console.WriteLine();}}
+
+int n = 11; int m = 11;
+int[,] matrix = new int[n, m];
+NewMatrix(matrix);
+PrintMatrix(matrix);
